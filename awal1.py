dcdec7bfba134ec2c32f0bcb8cfc973f02bc43df
unknown <nawaalhanif@gmail.com>
2024-09-25 11:59:07 +0700

    asdasd

--- awal1.py
+++ awal1.py
@@ -2,7 +2,7 @@ def fx4(x) :
     return x*x*x*x
 def max2(x,y) :
     return (int)((x + y) + (fx4(x - y))**0.5)
-print(max2(100,200)) asdasd
+print(max2(100,200)) asdasd adsasd
 
 def isOrigin (absis,ordinat) :
     """hgjhgjhhjg
